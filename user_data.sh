a927db83ab356f79cacefba57814bb2cfbf84422
Ravi Yadav <ravidv2000@gmail.com>
2024-05-11 20:12:20 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -2,7 +2,9 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install -y python3-pip
-pip3 install flask boto3 pymysql --break-system-packages
+pip3 install flask 
+pip3 install boto3
+pip3 install pymysql
 git clone https://github.com/raviyadav1907/Assignment-AWS-NAGP.git
 cd Assignment-AWS-NAGP
 python3 app.py
